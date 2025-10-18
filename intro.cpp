746c1b357bd18731934231a19da099574be120be
Suhad Rahman <suhadturan03@gmail.com>
2025-10-18 12:58:01 +0600

    Introduction using c++

---
+++ intro.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    cout<<"Hi, there!"<<endl;
+    cout<<"This is Suhad Rahman"<<endl;
+    cout<<"This is my first introduction code using c++"<<endl;
+    cout<<"Nice to meet you!!"<<endl;
+    
+    
+    return 0;
+}

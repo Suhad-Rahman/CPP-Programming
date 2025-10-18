5d8d2128953b0757424f2725aee91849dca3d475
Suhad Rahman <suhadturan03@gmail.com>
2025-10-18 12:51:12 +0600

    User input using 'cout'

---
+++ user_input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    
+    cout<<"Enter 2 number to Sum: ";
+    //scanf can also be used to take inputs
+    cin>>x>>y;
+    
+    cout<<"The sum is= "<<x+y<<endl;
+    
+    
+    
+    
+}
